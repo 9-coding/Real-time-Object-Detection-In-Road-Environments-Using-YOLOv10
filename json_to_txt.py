603530d75ede8fc4938774ad25ba18ffae88b5b6
9-coding <9.coding.lab@gmail.com>
2024-06-05 22:43:05 +0900

    FEAT: add yolo format bbox conversion code

--- json_to_txt.py
+++ json_to_txt.py
@@ -6,6 +6,17 @@ folder_list = ['test', 'train', 'val']
 if not os.path.exists("annotations"):
     os.mkdir("annotations")
 
+# xywh형식에서 주어진 데이터셋은 좌상단 xy, YOLO는 중심점 xy 사용.
+def find_center(bbox):
+    return (bbox[0]*2+bbox[2]) / 2, (bbox[1] * 2 + bbox[3]) / 2
+
+def normalize(x, y, w, h):
+    x = x/1280
+    y = y/720
+    w = w/1280
+    h = h/720
+    return x, y, w, h
+
 for folder in folder_list:
     path = 'dataset/' + folder + '/annotations'
     print(folder)
@@ -27,8 +38,24 @@ for folder in folder_list:
             for l in range(len(data['annotations'])):
                 category_id = data['annotations'][l]['category_id']
                 bbox = data['annotations'][l]['bbox']
-                #data = str(category_id)#+bbox[0]+bbox[1]+bbox[2]+bbox[3]
-                print(category_id, bbox)
-                bbox_str = ' '.join(map(str, bbox))
-                f.write(str(category_id)+' '+bbox_str+"\n")
+                x, y = find_center(bbox)
+                point_list = normalize(x, y, bbox[2], bbox[3])
+                points = ' '.join(map(str, point_list))
+                f.write(str(category_id) + ' ' + points + "\n")
             f.close()
+
+
+"""
+print(path+'/'+file_name + '.png')
+            img = cv2.imread('dataset/' + folder + '/images/'+file_name + '.png')
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            plt.imshow(img)
+            plt.scatter(bbox[0], bbox[1])
+            plt.scatter(bbox[0]+bbox[2], bbox[1]+bbox[3])
+            x, y = find_center(bbox)
+            #x, y, w, h = normalize(x, y, bbox[2], bbox[3])
+            point_list = normalize(x, y, bbox[2], bbox[3])
+            print(point_list)
+            plt.scatter(x, y)
+
+            plt.show()"""
